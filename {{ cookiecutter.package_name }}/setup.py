92afe729f93170eeac3162bcac8c085d8ae12c54
Florian Haas <florian@citynetwork.eu>
2022-03-10 17:12:37 +0100

    build: Drop support for Python<3.8
    Tutor became the official deployment facility for Open edX with the
    Maple release. Maple relies on Python 3.8, so remove everything that
    claims or tests compatibility with earlier versions.

--- {{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.package_name }}/setup.py
@@ -25,7 +25,7 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=["tutor"],
     setup_requires=["setuptools-scm"],
     entry_points={
@@ -39,8 +39,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
