3ac33cf3f07ef04a0a96bfc64ec780368982f2c0
Kyle McCormick <kyle@kylemccormick.me>
2022-07-14 15:08:16 -0400

    docs: make default plugin.py less magical
    The easiest way to upgrade from the V0->V1 plugin API
    is to make some trivial changes to plugin.py and then
    paste in couple dozen lines of boilerplate code that which
    convert the old V0 ``config`` dictionary into proper
    V1 filter callbacks.
    
    Since the V1 plugin API was released back in ~March,
    we have essentially mimicked this pattern in the plugin
    cookiecutter repo's plugin.py file.
    
    A downside of this, however, is that it obscures away the
    V1 plugin API, and encourages the plugin developer to just
    "trust" the boilerplate data manipulations that the plugin
    cookiecutter is serving them.
    
    In this commit, we break away from that, instead putting Filters
    from the V1 plugin API front-and-center. The hope is to
    entice developers to understand the new plugin API whilst
    ensuring it remains easy to stand up a simple plugin with the
    cookiecutter.

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/plugin.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/plugin.py
@@ -7,31 +7,43 @@ from tutor import hooks
 from .__about__ import __version__
 
 
-################# Configuration
-config = {
-    # Add here your new settings
-    "defaults": {
-        "VERSION": __version__,
-    },
-    # Add here settings that don't have a reasonable default for all users. For
-    # instance: passwords, secret keys, etc.
-    "unique": {
-        # "SECRET_KEY": "{{ '{{' }} 24|random_string {{ '}}' }}",
-    },
-    # Danger zone! Add here values to override settings from Tutor core or other plugins.
-    "overrides": {
-        # "PLATFORM_NAME": "My platform",
-    },
-}
+################# CONFIGURATION
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        # Add here your new settings that have default values.
+        # Each new setting is a pair: (setting_name, default_value).
+        # Prefix your setting names with '{{ cookiecutter.plugin_name|upper|replace('-', '_') }}_'.
+        ("{{ cookiecutter.plugin_name|upper|replace('-', '_') }}_VERSION", __version__),
+    ]
+)
+hooks.Filters.CONFIG_UNIQUE.add_items(
+    [
+        # Add here settings that don't have a reasonable default for all users.
+        # For instance: passwords, secret keys, etc.
+        # Each new setting is a pair: (setting_name, unique_generated_value).
+        # Prefix your setting names with '{{ cookiecutter.plugin_name|upper|replace('-', '_') }}_'.
+        # For example:
+        # ("{{ cookiecutter.plugin_name|upper|replace('-', '_') }}_SECRET_KEY", "{{ '{{' }} 24|random_string {{ '}}' }}"),
+    ]
+)
+hooks.Filters.CONFIG_OVERRIDES.add_items(
+    [
+        # Danger zone! Add here values to override settings from Tutor core or other plugins.
+        # Each override is a pair: (setting_name, new_value). For example:
+        # ("PLATFORM_NAME", "My platform"),
+    ]
+)
 
-################# Initialization tasks
+
+################# INITIALIZATION TASKS
 # To run the script from templates/{{ cookiecutter.plugin_name }}/tasks/myservice/init, add:
 # hooks.Filters.COMMANDS_INIT.add_item((
 #     "myservice",
 #     ("{{ cookiecutter.plugin_name }}", "tasks", "myservice", "init"),
 # ))
 
-################# Docker image management
+
+################# DOCKER IMAGE MANAGEMENT
 # To build an image with `tutor images build myimage`, add a Dockerfile to templates/{{ cookiecutter.plugin_name }}/build/myimage and write:
 # hooks.Filters.IMAGES_BUILD.add_item((
 #     "myimage",
@@ -50,20 +62,29 @@ config = {
 # )
 
 
-################# You don't really have to bother about what's below this line,
-################# except maybe for educational purposes :)
-
-# Plugin templates
-hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
-    pkg_resources.resource_filename("{{ cookiecutter.module_name }}", "templates")
+################# TEMPLATE RENDERING
+################# (It is safe & recommended to leave this section as-is :)
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_items(
+    # Root paths for template files, relative to the project root.
+    [
+        pkg_resources.resource_filename("{{ cookiecutter.module_name }}", "templates"),
+    ]
 )
 hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    # For each pair (source_path, destination_path):
+    #   templates at ``source_path`` (relative to your ENV_TEMPLATE_ROOTS) will rendered
+    #   to ``destination_path`` (relative to your Tutor environment).
     [
         ("{{ cookiecutter.plugin_name }}/build", "plugins"),
         ("{{ cookiecutter.plugin_name }}/apps", "plugins"),
     ],
 )
-# Load all patches from the "patches" folder
+
+
+################# PATCH LOADING
+################# (It is safe & recommended to leave this section as-is :)
+# For each file in {{ cookiecutter.module_name }}/patches,
+# apply a patch based on the file's name and contents.
 for path in glob(
     os.path.join(
         pkg_resources.resource_filename("{{ cookiecutter.module_name }}", "patches"),
@@ -72,18 +93,3 @@ for path in glob(
 ):
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
-
-# Load all configuration entries
-hooks.Filters.CONFIG_DEFAULTS.add_items(
-    [
-        (f"{{ cookiecutter.plugin_name|upper|replace('-', '_') }}_{key}", value)
-        for key, value in config["defaults"].items()
-    ]
-)
-hooks.Filters.CONFIG_UNIQUE.add_items(
-    [
-        (f"{{ cookiecutter.plugin_name|upper|replace('-', '_') }}_{key}", value)
-        for key, value in config["unique"].items()
-    ]
-)
-hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
