f567efd5b9e0bc15bcfc7fd072f8091ce5654a2f
Emad Rad <codewithemad@gmail.com>
2023-12-26 14:55:55 +0330

    fix: tutor supports python >= 3.8

--- {{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.package_name }}/setup.py
@@ -68,7 +68,6 @@ setup(
         {%- endif %}
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
